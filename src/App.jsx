0673a0395c8f91e93981d6325e68ae31dd97a6ae
mason-san <mazinmoosa63@gmail.com>
2026-01-06 15:21:13 +0530

    Add attendance calculator UI skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
 import './App.css'
+import "./index.css"
+import { useState } from "react";
 
 function App() {
+  const [totalClasses, setTotalClasses] = useState("");
+  const [attendedClasses, setAttendedClasses] = useState("");
+  const [requiredPercentage, setRequiredPercentage] = useState("");
+
   return (
-    <>
-    <div>
-       Attendance Calculator
+    <div className='app'>
+      <div className='title'>Attendance Calculator</div>
+
+      <div className='card'>
+        <label>Total Number of classes</label>
+        <input 
+          type='number'
+          value={totalClasses}
+          onChange={(e) => setTotalClasses(e.target.value)}
+          />
+
+        <label>Classes attended</label>
+        <input
+          type='number'
+          value={attendedClasses}
+          onChange={(e) => setAttendedClasses(e.target.value)}
+          />
+
+        
+        <label>Required Classes</label>
+        <input  
+          type='number'
+          value={requiredPercentage}
+          onChange={(e) => setRequiredPercentage(e.target.value)}
+          />
+
+        <button className='calculate-btn'>Calculate</button>
+
+        <div className='divider' />
+        <p className='placeholder'>
+          Enter your details and click calculate
+        </p>
+      </div>
     </div>
-    </>
-  )
+  );
 }
 
 export default App
